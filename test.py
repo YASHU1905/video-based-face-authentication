d64b977992b7a11bcb3b5bc8422dc42b51b86738
YASHU1905 <42761437+YASHU1905@users.noreply.github.com>
2020-03-06 13:11:54 +0530

    Create test.py

---
+++ test.py
@@ -0,0 +1,11 @@
+import cv2
+import dlib
+import tensorflow as tf
+import numpy as np
+import pandas as pd
+
+print(dlib.__version__)
+print(cv2.__version__)
+print(tf.__version__)
+print(np.__version__)
+print(pd.__version__)
